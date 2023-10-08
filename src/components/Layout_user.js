67f51ee878c8ca3b4f8353b1e532648695d7bc78
DavidVela3003 <20202209@aloe.ulima.edu.pe>
2023-10-07 20:04:02 -0500

    hola

--- src/components/Layout_user.js
+++ src/components/Layout_user.js
@@ -1,23 +1,22 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
-export default props => (
+export default props1 => (
     <>
         <header>
         </header>
         <nav>
-
             <ul>
-                <li><a href="/admin">Inicio</a></li>
-                <li><a href="/perfil_user">Perfil</a></li>
-                <li><a href="/prestamos">Prestamos</a></li>
+                <li><Link href="/user">Inicio</Link></li>
+                <li><Link href="/perfil_user">Perfil</Link></li>
+                <li><Link href="/prestamos">Prestamos</Link></li>
             </ul>
         </nav>
 
 
 
         <main>
-            {props.content}
+            {props1.content}
         </main>
         <footer>
             <p>Ingenieria de Sistemas &copy; 2023-2 - Programacion Web 704-Grupo 1</p>
