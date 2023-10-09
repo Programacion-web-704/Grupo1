04db294fc159f698596ee5d4f6af431b06657cb0
DavidVela3003 <139900862+DavidVela3003@users.noreply.github.com>
2023-10-09 16:36:22 -0500

    user.js

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,12 +1,15 @@
 import Link from "next/link"
 import Head from 'next/head'
 import Layout from '../components/Layout_user.js'
+import Image from "next/image.js"
 
 const Index2 = () => <Layout content={
 
     <>
         <div>
             <h1>Bienvenido usuario</h1>
+            <h4><p>Ultimas reservas</p></h4>
+            <Image src="/img3.jpg" width={200} height={300}></Image>
         </div>
 
     </>
