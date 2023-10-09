0b8714125a58616aa311ef71bc7089b05c55ba09
DavidVela3003 <139900862+DavidVela3003@users.noreply.github.com>
2023-10-09 16:36:00 -0500

    admin.js

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,12 +1,16 @@
 import Link from "next/link"
 import Head from 'next/head'
 import Layout from '../components/Layout_admin.js'
+import Image from "next/image.js"
 
 const Index = () => <Layout content={
 
     <>
     <div>
         <h1>Bienvenido admin</h1>
+        <Image src="/img.png" width={200} height={300}></Image>
+
+
     </div>
     </>
 } />
