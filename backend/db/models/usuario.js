18f005092c379ca802e0079d91c3bda205c75dba
vilxleo <144192548+vilxleo@users.noreply.github.com>
2023-11-25 17:37:15 -0500

    Update usuario.js

--- backend/db/models/usuario.js
+++ backend/db/models/usuario.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Usuario.hasMany(models.Reserva, { foreignKey: 'usuario_id_usuario' }); 
     }
   }
   Usuario.init({
