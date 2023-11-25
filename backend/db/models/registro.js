6a2b41e2c2670b56a9a46d64015fdaf8a27b2af6
vilxleo <144192548+vilxleo@users.noreply.github.com>
2023-11-25 17:36:29 -0500

    Update registro.js

--- backend/db/models/registro.js
+++ backend/db/models/registro.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Registro.belongsTo(models.Admin, { foreignKey: 'id_registro' });
     }
   }
   Registro.init({
